f3e2c35491b4f35dab12dd40c52809fb72f0e34d
hamidoucheasmae <hamidouche.asmae@gmail.com>
2021-07-08 11:57:57 +0100

    + illustartion

--- Prototype - Code Source/api/getProduct.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../manager/productsManager.php';
-
-     $productsManager = null;
-        $productsManager = new ProductsManager();    
-        $products = $productsManager->getList();
-        print_r(json_encode($products));
-?>
-  
