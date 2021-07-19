67256f4f4ae706ed53ffc5ea9cc8ece3cbee10e0
hamidoucheasmae <hamidouche.asmae@gmail.com>
2021-07-19 08:21:08 +0100

    sql update

--- realisation/PHP-MySQL-User-Signup-Login-API-master/PHPLearning.sql
+++ realisation/PHP-MySQL-User-Signup-Login-API-master/PHPLearning.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: May 25, 2018 at 12:09 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
+-- Host: localhost:8889
+-- Generation Time: Jul 19, 2021 at 07:19 AM
+-- Server version: 5.7.32
+-- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,16 +30,18 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
+  `email` varchar(55) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `created`) VALUES
-(1, 'shehryar', 'MTIzNDU=', '2018-05-25 06:17:14');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `created`) VALUES
+(1, 'shehryar', '0', 'MTIzNDU=', '2018-05-25 06:17:14'),
+(2, 'asmae_mid', 'hamidouche@gmail.com', 'MTIzNDU2', '2021-07-19 07:17:03');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +61,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
